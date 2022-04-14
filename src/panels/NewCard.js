2e61231e6af80ec53b5947c13b2952293ff32307
Arseny Tsivarev <me@xvtn.ru>
2022-04-14 23:00:22 +0500

    @nitroauth > @tsivxrev

--- src/panels/NewCard.js
+++ src/panels/NewCard.js
@@ -19,7 +19,7 @@ const NewCard = (id) => {
           <Text weight="regular" style={{ marginBottom: 9 }}>
             Неофициальный сервис для просмотра баланса транспортных карт
             Тюменской области. Если есть вопросы по поводу данного приложения,
-            то напишите мне в личку @nitroauth
+            то напишите мне в личку @tsivxrev
           </Text>
           <Subhead weight="regular" style={{ color: 'var(--text_secondary)' }}>
             Для продолжения добавьте свою транспортную карту
